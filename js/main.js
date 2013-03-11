e9ccfa9bd742782fa986815f599532b0e38cad1d
Jared A. Scheel <jared@jaredscheel.com>
2013-03-11 17:58:42 -0500

    fixed fancybox and cycle

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
 $(document).on('pop-initialized', function(){
-
-  $('.asset-type-imagegroup').live('initialize', function(e, asset){
-  	$(this).find('.slideshow .images').cycle({
-				fx: 'fade',
-				timeout: Math.floor(Math.random() * 8000) + 3000,
-				delay: Math.floor(Math.random() * 6000) + 1000
-	    });
-  });
-
-  $('.asset').live('initialize', function(e, asset){
-		$("a.fancybox").fancybox();
-	}).live('destroy', function(e, asset){
-		$("a.fancybox").fancybox();
-  });
-
-
 });
 
 
